5c43cff40cdf8fa1ab82a1f695e0c4e700c8d3ea
Jared <youngblipp@gmail.com>
2023-12-04 01:14:35 -0500

    - Media Query addition - Reflections Update - Secondary Color Change - Code Cleanup - Solution Functionality addition - Nav Rearrangment

---
+++ index.js
@@ -0,0 +1,23 @@
+const s_1 = document.querySelector("#s_1")
+const s_2 = document.querySelector("#s_2")
+const s_3 = document.querySelector("#s_3")
+const s_4 = document.querySelector("#s_4")
+const s_5 = document.querySelector("#s_5")
+
+const text_box = document.querySelector("#text_box")
+
+s_1.addEventListener("click", function() {
+    text_box.textContent = "Many devices that allow for the use of social media apps include a feature with the ability to self impose a screen time limit. This use of a limit helps to reduce the time spent on social media thus forcing its user to spend their time in a different way curbing their addiction to their device. While it can easily be bypassed or ignored, setting a time limit will help keep the user self aware as to how much time they spent on social media"
+})
+s_2.addEventListener("click", function() {
+    text_box.textContent = "Online deprecation and cyber-harassment can be addressed using many different facets. Legal measures may be used to create a framework to prosecute serious offenders and deter malicious online behavior. Technological solutions like content moderation systems and anonymity controls may be used to mitigate cyber-harassment. Community education fosters a responsible online culture, allowing users to participate in online communities safely and positively. Following community rules and agreements promotes positive online interactions and holding individuals accountable."
+})
+s_3.addEventListener("click", function() {
+    text_box.textContent = "Websites can provide filtering and age restricting for anything that is deemed inappropriate. Additionally, parents can monitor their child’s online presence by being aware of the content typically shared on those platforms, or material expressed within the content they engage with. Parents can also communicate with their children on what is considered inappropriate, and how to react when they are exposed to it."
+})
+s_4.addEventListener("click", function() {
+    text_box.textContent = "Parents must take charge and help their children develop a healthy relationship with the internet and technology. As well as possibly including a section for elementary computer classes to include proper ways to allow the children to limit themselves on the way they interact with technology. It is the choice of the child to make a healthy balance, but it is up to the people raising and teaching them to help them fully understand the importance of this balance."
+})
+s_5.addEventListener("click", function() {
+    text_box.textContent = "One solution to the security issue would be incorporating digital literacy and online safety education into school curricula. This includes teaching children about the potential risks of sharing personal information online, recognizing cyber threats, and instilling responsible online behavior. Additionally, engaging parents in the learning will create a collaborative effort between the schools and the parents"
+})
